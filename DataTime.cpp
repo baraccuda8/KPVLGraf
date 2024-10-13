8e0425b9cbd92ca66c085431f6b2c29fcd95d5ae
Azarov <baraccuda@mail.ru>
2024-10-14 01:19:55 +0500

    Поправил даты

--- DataTime.cpp
+++ DataTime.cpp
@@ -6,44 +6,35 @@ time_t DataTimeOfString(std::string str)
 {
     try
     {
-        std::tm TM;
-        TM.tm_year = 0; TM.tm_mon = 0; TM.tm_mday = 0; TM.tm_hour = 0; TM.tm_min = 0; TM.tm_sec = 0;
+        std::tm TM ={0,0,0,0,0,0,0,0,0};
         std::string::const_iterator start = str.begin();
         std::string::const_iterator end = str.end();
-        boost::regex xRegEx(FORMATTIME1);
         boost::match_results<std::string::const_iterator> what;
 
-        if(boost::regex_search(start, end, what, xRegEx, boost::match_default))
+        boost::regex xRegEx(FORMATTIME1);
+        if(boost::regex_search(start, end, what, xRegEx, boost::match_default) && what.size() > 6)
         {
-            size_t size = what.size();
-            if(size > 6)
-            {
-                if(size > 1)TM.tm_year = Stoi(what[1].str()) - 1900;
-                if(size > 2)TM.tm_mon = Stoi(what[2].str()) - 1;
-                if(size > 3)TM.tm_mday = Stoi(what[3].str());
-                if(size > 4)TM.tm_hour = Stoi(what[4].str());
-                if(size > 5)TM.tm_min = Stoi(what[5].str());
-                if(size > 6)TM.tm_sec = Stoi(what[6].str());
-            }
+            TM.tm_year = Stoi(what[1]) - 1900;
+            TM.tm_mon = Stoi(what[2]) - 1;
+            TM.tm_mday = Stoi(what[3]);
+            TM.tm_hour = Stoi(what[4]);
+            TM.tm_min = Stoi(what[5]);
+            TM.tm_sec = Stoi(what[6]);
             return mktime(&TM);
         }
         else
         {
-            boost::regex xRegEx(FORMATTIME2);
+            xRegEx = FORMATTIME2;
             boost::match_results<std::string::const_iterator> what;
 
-            if(boost::regex_search(start, end, what, xRegEx, boost::match_default))
+            if(boost::regex_search(start, end, what, xRegEx, boost::match_default) && what.size() > 6)
             {
-                size_t size = what.size();
-                if(size > 6)
-                {
-                    if(size > 1)TM.tm_mday = Stoi(what[1].str());
-                    if(size > 2)TM.tm_mon = Stoi(what[2].str()) - 1;
-                    if(size > 3)TM.tm_year = Stoi(what[3].str()) - 1900;
-                    if(size > 4)TM.tm_hour = Stoi(what[4].str());
-                    if(size > 5)TM.tm_min = Stoi(what[5].str());
-                    if(size > 6)TM.tm_sec = Stoi(what[6].str());
-                }
+                TM.tm_mday = Stoi(what[1]);
+                TM.tm_mon = Stoi(what[2]) - 1;
+                TM.tm_year = Stoi(what[3]) - 1900;
+                TM.tm_hour = Stoi(what[4]);
+                TM.tm_min = Stoi(what[5]);
+                TM.tm_sec = Stoi(what[6]);
                 return mktime(&TM);
             }
         }
--- DataTime.h
+++ DataTime.h
@@ -1,5 +1,5 @@
 #pragma once
-const std::string FORMATTIME1 = "(\\d{4}).(\\d{2}).(\\d{2}) (\\d{2}):(\\d{2}):(\\d{2})*";
-const std::string FORMATTIME2 = "(\\d{2}).(\\d{2}).(\\d{4}) (\\d{2}):(\\d{2}):(\\d{2})*";
+const std::string FORMATTIME1 = "^(\\d{4})-(\\d{2})-(\\d{2}) (\\d{2}):(\\d{2}):(\\d{2})*";
+const std::string FORMATTIME2 = "^(\\d{2})-(\\d{2})-(\\d{4}) (\\d{2}):(\\d{2}):(\\d{2})*";
 
 time_t DataTimeOfString(std::string str);
